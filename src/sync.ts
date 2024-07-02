ce2ed88e30fe45c58291dfad8eccc7ff7351ef03
Josh Morrow <josh@jcmorrow.com>
2024-07-02 11:44:08 -0700

    Try this

--- src/sync.ts
+++ src/sync.ts
@@ -17,6 +17,7 @@ export async function downloadAndSaveQAWolfTests() {
 
   return tests.workflowsOnBranch
     .filter((t) => t.workflow.status === "active")
+    .slice(0, 1)
     .map((test) => {
       const helperCode =
         test.stepOnBranchInWorkflowOnBranch
